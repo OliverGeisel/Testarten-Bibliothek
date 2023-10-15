df022432e39b4a47f27e4717c5d8ac7f365deb6b
Oliver Geisel <geisel.oliver@googlemail.com>
2023-10-15 02:12:21 +0200

    Initial commit for integration test part.

---
+++ test-arten/integration-tests/src/main/java/de/olivergeisel/integrationtests/IntegrationtestsApplication.java
@@ -0,0 +1,13 @@
+package de.olivergeisel.integrationtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationtestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationtestsApplication.class, args);
+	}
+
+}
